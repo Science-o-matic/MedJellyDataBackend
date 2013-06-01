144c9941718a67335252d9f0569ebd38c80b39e6
ygneo <antonio.barcia@gmail.com>
2013-06-02 01:51:56 +0200

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,9 @@ from fabric.contrib.project import rsync_project
 from fab_settings import PROJECT_NAME, PROJECT_HOST, PROJECT_USER, PROJECT_DB_NAME, SUDOER_USER
 
 
-env.project_name = '<project_name>'
+env.forward_agent = True
+env.project_name = PROJECT_NAME
+env.project_user = PROJECT_USER
 env.roledefs = {
     PROJECT_USER: ['%s@%s' % (PROJECT_USER, PROJECT_HOST)],
     'sudoer': ['%s@%s' % (SUDOER_USER, PROJECT_HOST)]
@@ -27,14 +29,14 @@ def git_status():
 @roles('%s' % PROJECT_USER)
 def pushpull():
     local("git push origin master")
-    with settings(user='jellyrisk'):
+    with settings(user=PROJECT_USER):
         with cd(env['project_path']):
             run('git pull')
 
 
 @roles('sudoer')
 def reloadapp():
-    sudo('supervisorctl restart jellyrisk', shell=False)
+    sudo('supervisorctl restart %s' % env.project_name, shell=False)
     sudo('service nginx reload', shell=False)
 
 
