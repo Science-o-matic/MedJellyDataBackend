d8a2857fec1b37cf2ba6b5f37029beca89a5610a
ygneo <antonio.barcia@gmail.com>
2013-06-03 23:06:52 +0200

    Translated all models

--- sights/models.py
+++ sights/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 from django.db import models
 
@@ -10,6 +12,9 @@ class Sight(models.Model):
     def __unicode__(self):
         return "%s: %s (%s)" % (self.timestamp, self.beach, self.beach_code)
 
+    class Meta:
+        verbose_name = "Avistamiento"
+
 
 class Beach(models.Model):
     code = models.CharField(max_length=20)
@@ -18,12 +23,19 @@ class Beach(models.Model):
     def __unicode__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Playa"
+
 
 class VariablesGroup(models.Model):
     name = models.CharField(max_length=1000)
 
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "Grupo de Variables"
+        verbose_name_plural = "Grupos de Variables"
 
 
 class VariableType(models.Model):
@@ -36,6 +48,10 @@ class VariableType(models.Model):
     def __unicode__(self):
         return "%s - %s" % (self.name, self.description)
 
+    class Meta:
+        verbose_name = "Tipo de Variable"
+        verbose_name_plural = "Tipos de Variables"
+
 
 class VariableDescription(models.Model):
     variable_type = models.ForeignKey('VariableType', related_name='possible_descriptions')
@@ -44,12 +60,21 @@ class VariableDescription(models.Model):
     def __unicode__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Descripción de Variable"
+        verbose_name_plural = "Descripciones de Variables"
+
+
 
 class MeasureUnits(models.Model):
     name = models.CharField(max_length=1000)
 
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "Unidad de medida"
+        verbose_name_plural = "Unidades de medida"
 
 
 class SightVariables(models.Model):
@@ -59,3 +84,8 @@ class SightVariables(models.Model):
 
     def __unicode__(self):
         return "%s %s %s" % (self.sight, self.variable, self.value)
+
+
+    class Meta:
+        verbose_name = "Variable de avistamiento"
+        verbose_name_plural = "Variables de avistamientos"
