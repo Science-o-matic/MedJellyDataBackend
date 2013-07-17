ce4b1f937a744208961cd086628df96a35fc3ba0
ygneo <antonio.barcia@gmail.com>
2013-07-17 23:49:15 +0200

    Client-side new sight form validation

--- sights/static/js/new_sight.js
+++ sights/static/js/new_sight.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
- $.mobile.ajaxEnabled = false;
+
+  $("form").submit(function (e) {
+    if ($("#id_var_63").val().trim() === "") {
+      var errorMessage = "<p class=\"input-error\">Has d'indicar la temperatura de l'aigua</p>"
+
+      $(".input-error").remove();
+      $("#id_var_63").parent().parent().append(errorMessage);
+      e.preventDefault();
+    }
+  });
+
+  $.mobile.ajaxEnabled = false;
 
   if ($("#message").length) {
     $("#message").fadeOut(2400);
