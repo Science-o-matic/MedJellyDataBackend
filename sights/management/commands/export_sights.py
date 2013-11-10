95aa6827285ce571f83889f415b404888cef4906
ygneo <antonio.barcia@gmail.com>
2013-11-11 00:09:00 +0100

    Add jellyfish size ("grandaria") column for all jellyfishes in export sights mng command

--- sights/management/commands/export_sights.py
+++ sights/management/commands/export_sights.py
@@ -12,6 +12,7 @@ class Command(BaseCommand):
     help = 'Export all sights in a CSV file'
     abundance_keyword = "Abundància"
     presence_keyword = "Presència"
+    size_keyword = "Grandària"
     presence_variable_label = "Meduses"
     presence_variable_value = "1.00"
     jellyfishes_names = ["Pelagia", "Aurelia", "Cotylorhiza", "Rhizostoma", "Chrysaora",
@@ -38,6 +39,7 @@ class Command(BaseCommand):
     def write_header(self, csvwriter):
         header_cols = ["beach", "date", "day", "month", "year"]
         header_cols += self.jellyfishes_names
+        header_cols += ["%s grandaria" % s for s in self.jellyfishes_names]
         header_cols += ["Origen",
                         "Bandera", "Motivo Bandera",
                         "T. Agua",
@@ -51,6 +53,7 @@ class Command(BaseCommand):
         try:
             data = [sight.beach, date.strftime("%m/%d/%Y"), date.day, date.month, date.year]
             data += self.jellyfish_values(sight)
+            data += self.jellyfish_size_values(sight)
             data += [sight.reported_from, int(sight.get_flag()), sight.get_flag_reason()]
             data += [self._get_water_temperature(sight)]
             data += [",".join(self._get_metereology_variables_values(sight))]
@@ -80,19 +83,27 @@ class Command(BaseCommand):
         # 2 - presencia con abundancia bastantes
         # 3 - presencia con abundancia muchas
         # NA - sin datos
-
-        sight_has_jellyfishes = sight.sightvariables_set.filter(
-            variable__variable__label=self.presence_variable_label,
-            value=self.presence_variable_value
-        )
-        jellyfishes_presence = bool(sight_has_jellyfishes)
-
-        if jellyfishes_presence:
+        if self._jellyfishes_presence(sight):
             values = self.jellyfishes_abundance(sight)
         else:
             values = [0 for name in self.jellyfishes_names]
 
         return values
+
+    def jellyfish_size_values(self, sight):
+        if self._jellyfishes_presence(sight):
+            values = self.jellyfishes_size(sight)
+        else:
+            values = [0 for name in self.jellyfishes_names]
+
+        return values
+
+    def _jellyfishes_presence(self, sight):
+        sight_has_jellyfishes = sight.sightvariables_set.filter(
+            variable__variable__label=self.presence_variable_label,
+            value=self.presence_variable_value
+        )
+        return bool(sight_has_jellyfishes)
 
     def jellyfishes_abundance(self, sight):
         jellyfishes_abundance_vars = sight.sightvariables_set.filter(
@@ -118,6 +129,32 @@ class Command(BaseCommand):
                 else:
                     values.append(0)
         return values
+
+    def jellyfishes_size(self, sight):
+        jellyfishes_size_vars = sight.sightvariables_set.filter(
+            variable__variable__label__contains=self.size_keyword
+            )
+        jellyfishes_size_vars = jellyfishes_size_vars.values_list(
+            "variable__variable__label",
+            "value"
+            )
+        jellyfishes_size = {}
+        for var in jellyfishes_size_vars:
+            key = var[0].split("-")[0].split(" ")[0].strip()
+            value = int(var[1]) if int(var[1]) > 0 else "NA"
+            jellyfishes_size[key] = value
+
+        values = []
+        for jellyfish_name in self.jellyfishes_names:
+            try:
+                values.append(jellyfishes_size[jellyfish_name])
+            except KeyError:
+                if self.jellyfishes_presence(jellyfish_name, sight):
+                    values.append("NA")
+                else:
+                    values.append(0)
+        return values
+
 
     def jellyfishes_presence(self, name, sight):
         jellyfishes_presence_vars = sight.sightvariables_set.filter(
