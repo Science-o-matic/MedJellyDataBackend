21df069dd785afcc5d35ea643b8d577f8307b715
ygneo <antonio.barcia@gmail.com>
2014-05-07 12:44:24 +0200

    Disable submit button while sending sight report

--- sights/static/js/new_sight.js
+++ sights/static/js/new_sight.js
@@ -1,12 +1,33 @@
+function toggleSubmitButton() {
+  var span_button = $('.ui-submit span.ui-btn-inner'),
+    submit_button = $('.ui-submit input[type=submit]'),
+    span_text = $(".ui-submit .ui-btn-text");
+
+  if (submit_button.attr('disabled')) {
+    submit_button.removeAttr('disabled');
+    span_button.removeClass('ui-disabled');
+    span_text.text(this.text);
+  } else {
+    this.text = span_text.text();
+    submit_button.attr('disabled', 'disabled');
+    span_button.addClass('ui-disabled');
+    span_text.text("Enviando...");
+  }
+}
+
+
 $(document).ready(function () {
 
   $("form").submit(function (e) {
+    toggleSubmitButton();
+
     if ($("#id_var_63").val().trim() === "") {
       var errorMessage = "<p class=\"input-error\">Has d'indicar la temperatura de l'aigua</p>"
 
       $(".input-error").remove();
       $("input[type=submit]").parent().parent().append(errorMessage);
       e.preventDefault();
+      toggleSubmitButton();
     }
   });
 
--- sights/urls.py
+++ sights/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
-#                       url(r'new/', NewSightView.as_view())
-                       url(r'new/', "sights.views.new")
+                       url(r'^new/', "sights.views.new")
              )
