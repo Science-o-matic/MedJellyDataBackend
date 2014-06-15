48b5989a7e34082fffd749dbfd3cbb1ff870ac29
Antonio Barcia García <antonio.barcia@gmail.com>
2014-06-15 19:35:35 +0200

    Fix jellyfishes fieldset cleaning when clicking jellyfishes presence checkbox

--- sights/static/js/new_sight.js
+++ sights/static/js/new_sight.js
@@ -50,7 +50,7 @@ function toggleJellyfishesFieldset(jellyfishes_presence, jellyfishes) {
         renderJellyfishes($(".jellyfishes"), jellyfishes);
     } else {
         $(".jellyfishes").hide();
-        $(".jellyfishes ul").remove();
+        $(".jellyfishes .jellyfish").remove();
     }
 }
 
