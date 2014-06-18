47a4a35b836c96f34989aba24b27cf5e4e10ac31
ygneo <antonio.barcia@gmail.com>
2014-06-18 11:36:45 +0200

    Add management command importing beaches from MedJelly API

---
+++ sights/management/commands/import_medjelly_beaches.py
@@ -0,0 +1,45 @@
+import os
+import requests
+import base64
+from xml.etree import ElementTree
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from sights.models import MedJellyBeach
+
+
+class Command(BaseCommand):
+    help = "Import beaches from MedJelly API to MedJellyBeach model"
+    enviroment = "production"
+
+    def handle(self, *args, **options):
+        url = os.path.join(
+            settings.MEDJELLY_API[self.enviroment]["url"],
+            "beaches/locations"
+        )
+        headers = {
+            "Authorization": self._auth_string()
+        }
+
+        print "GET %s" % url
+        response = requests.get(url, headers=headers)
+        print "Importing..."
+        count = self._import_beaches(response.json())
+        print "Imported", count, "beaches"
+
+
+    def _auth_string(self):
+        # TODO: Refactor this into a class wrapping MedJellyAPI, and use it here and in APIExporter
+        username = settings.MEDJELLY_API[self.enviroment]["user"]
+        password = settings.MEDJELLY_API[self.enviroment]["password"]
+        return "Basic %s" % base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+
+    def _import_beaches(self, data):
+        count = 0
+        for beach in data["beaches"]:
+            count += 1
+            MedJellyBeach.objects.get_or_create(
+                id=beach["id"],
+                name=beach["name"],
+                town=beach["municipalityName"]
+            )
+        return count
