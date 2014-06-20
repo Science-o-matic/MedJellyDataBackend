2c34001892d59f34b4db3604d366ec4936e04030
ygneo <antonio.barcia@gmail.com>
2014-06-20 11:25:35 +0200

    Modify logging formatter to drop process and thread info

--- settings.py
+++ settings.py
@@ -173,7 +173,7 @@ LOGGING = {
     'disable_existing_loggers': True,
     'formatters': {
         'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
