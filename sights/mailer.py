bafed0779ad87771a43123c6199c5655c2398d53
ygneo <antonio.barcia@gmail.com>
2014-08-04 11:26:53 +0200

    Add Martin Fuentes' email to import report emails in mailer

--- sights/mailer.py
+++ sights/mailer.py
@@ -7,7 +7,7 @@ from django.contrib.sites.models import Site
 CURRENT_SITE = Site.objects.get(pk=settings.SITE_ID)
 
 IMPORT_REPORT_MAILS = ('marambio@icm.csic.es', 'lopezcastillo89@gmail.com', 'veronica.jellyrisk@gmail.com',
-                       'antonio.barcia@gmail.com', 'maca.jellyrisk@gmail.com',
+                       'antonio.barcia@gmail.com', 'maca.jellyrisk@gmail.com', 'fuentesmartin@gmail.com'
                        )
 
 def notify_new_sighting(sight):
